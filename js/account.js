a6198fe3c136a26c258e488578fd14c6ecc0da5d
Megaju <julien_malle@outlook.fr>
2016-10-19 15:11:16 +0200

    possible de trier par groupe les users

---
+++ js/account.js
@@ -0,0 +1,7 @@
+var app = angular.module('appDette', []);
+app.controller('accountCtrl', function($scope) {
+
+    $scope.name = "Julien";
+    $scope.pwd = "root";
+    
+});
---
+++ js/users.js
@@ -0,0 +1,54 @@
+app.controller('usersCtrl', function($scope) {
+    
+    // les personnes enregistrées
+    $scope.users = [
+        {
+            name:'Julien',
+            debt:40,
+            pickup:75,
+            group:'Groupe 1'
+        },
+        {
+            name:'Lise',
+            debt:20,
+            pickup:0,
+            group:'Groupe 1'
+        },
+        {
+            name:'Becca',
+            debt:20,
+            pickup:40,
+            group:'Groupe 1'
+        },
+        {
+            name:'Tyjow',
+            debt:400,
+            pickup:120,
+            group:'Groupe 3'
+        },
+        {
+            name:'Juliette',
+            debt:4000,
+            pickup:1,
+            group:'Groupe 2'
+        },
+        {
+            name:'Val',
+            debt:2,
+            pickup:4000,
+            group:'Groupe 2'
+        },
+        {
+            name:'Roro',
+            debt:5,
+            pickup:1,
+            group:'Par defaut'
+        },
+        {
+            name:'Toto',
+            debt:65481,
+            pickup:2561,
+            group:'Par defaut'
+        }
+    ]
+});
